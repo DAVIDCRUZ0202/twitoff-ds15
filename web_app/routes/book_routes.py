aeb0131259103a1a4e116d5b07a03f6755f0d6d1
David Cruz <dcman1211@gmail.com>
2020-06-15 15:46:43 -0400

    fix add books bugs

--- web_app/routes/book_routes.py
+++ web_app/routes/book_routes.py
@@ -8,11 +8,11 @@ book_routes = Blueprint("book_routes", __name__)
 
 @book_routes.route("/books.json")
 def list_books():
-    books = [
-       {"id": 1, "title": "Book 1"},
-       {"id": 2, "title": "Book 2"},
-       {"id": 3, "title": "Book 3"},
-    ]
+    #books = [
+    #    {"id": 1, "title": "Book 1"},
+    #    {"id": 2, "title": "Book 2"},
+    #    {"id": 3, "title": "Book 3"},
+    #]
     book_records = Book.query.all()
     print(book_records)
     books = parse_records(book_records)
@@ -20,11 +20,11 @@ def list_books():
 
 @book_routes.route("/books")
 def list_books_for_humans():
-    books = [
-       {"id": 1, "title": "Book 1"},
-       {"id": 2, "title": "Book 2"},
-       {"id": 3, "title": "Book 3"},
-    ]
+    #books = [
+    #    {"id": 1, "title": "Book 1"},
+    #    {"id": 2, "title": "Book 2"},
+    #    {"id": 3, "title": "Book 3"},
+    #]
     book_records = Book.query.all()
     print(book_records)
     books = parse_records(book_records) # optionally
@@ -37,7 +37,7 @@ def new_book():
 @book_routes.route("/books/create", methods=["POST"])
 def create_book():
     print("FORM DATA:", dict(request.form))
-    
+
     new_book = Book(title=request.form["book_title"], author_id=request.form["author_name"])
     db.session.add(new_book)
     db.session.commit()
