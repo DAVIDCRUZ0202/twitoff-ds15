a8f81e6be30433d140ecf126a4e6a0fde79ccd11
David Cruz <dcman1211@gmail.com>
2020-06-18 15:08:46 -0400

    checkpoint in auth bug fixes

--- web_app/__init__.py
+++ web_app/__init__.py
@@ -18,7 +18,7 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 def create_app():
     app = Flask(__name__)
 
-    app.config["SQLALCHEMY_DATABASE_URL"] = DATABASE_URL
+    app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URL
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.init_app(app)
     migrate.init_app(app, db)
